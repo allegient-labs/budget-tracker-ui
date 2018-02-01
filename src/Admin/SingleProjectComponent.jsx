f440fe3a99a606328fd5702a07e1930a4ba93d13
William McCracken <wmccracken@allegient.com>
2018-02-01 16:23:05 -0500

    Adds association, needs cleanup

--- src/Admin/SingleProjectComponent.jsx
+++ src/Admin/SingleProjectComponent.jsx
@@ -12,27 +12,36 @@ class SingleProjectComponent extends Component {
     this.state={
       showEdit: false,
       clients:[],
-      clientsDropDown:[]
+      clientsDropDown:[],
+      selectedDropdownURI:"",
+      clientURI:"",
+      linkedClient:{}
+
     }
     this.updateThing=this.updateThing.bind(this)
+    this.changeHandler=this.changeHandler.bind(this)
+    this.putClientAssociation=this.putClientAssociation.bind(this)
   }
 
   componentDidMount(){
     axios.get(API_URL+'/clients')
     .then((clients)=>{
-      console.log(clients)
       this.setState({clients: clients.data._embedded.clients}, ()=>{
       var arr = [];
       this.state.clients.map((client, i)=>{
-        arr.push({key:i, text:client.name, value:client.url})
+        arr.push({key:i, text:client.name, value:client._links.self.href})
       })
       this.setState({clientsDropDown:arr})
     })})
 
+    this.props.selectedProject._links?this.setState({clientURI:this.props.selectedProject._links.client.href},()=>{
+      axios.get(this.state.clientURI)
+      .then((client)=>{this.setState({linkedClient:client.data})})
+      .catch((err)=>{console.log(err)})
+    }):null
   }
 
   updateThing(url, closeFunc, payload){
-    console.log(payload)
     axios.put(url.href, payload)
     .then((res)=>{
       this.props.rerouteToSelectedProject(payload)
@@ -40,12 +49,29 @@ class SingleProjectComponent extends Component {
     closeFunc()
   }
 
+  changeHandler(evt, selection){
+    this.setState({selectedDropdownURI:selection.value})
+  }
+
+  putClientAssociation(){
+    axios({  method: 'put',
+    url: this.state.clientURI,
+    data: this.state.selectedDropdownURI,
+    headers:{'Content-Type':'text/uri-list'}})
+    .then((client)=>{console.log(client)})
+    .then(()=>{
+      axios.get(this.state.clientURI)
+      .then((client)=>{this.setState({linkedClient:client.data})})
+      .catch((err)=>{console.log(err)})     
+    })
+  }
+
   render() {
-    console.log(this.state.clientsDropDown)
+    console.log(this.state.linkedClient)
     return (
       <div>
         <Button color="blue" icon="arrow circle left" onClick={()=>{this.props.history.push('/admin/projects')}}></Button>
-        <h3>AdminManageSingleProjectComponent</h3>
+        <h3>Edit Project Details</h3>
         <div className="projects">
           <div>Name: {this.props.selectedProject.name}</div>
           <div>Description: {this.props.selectedProject.description}</div>
@@ -57,8 +83,11 @@ class SingleProjectComponent extends Component {
           <EditProjectCard thingName="Project" thing={this.props.selectedProject} updateF={this.updateThing}/>
           <Button color="red" icon='erase'></Button>          
         </div>
-        <Dropdown text='Set Client' options={this.state.clientsDropDown}/>
-        <Button color="green">Confirm</Button>
+        <h4>Current Client:</h4>
+        <div>{this.state.linkedClient.name}</div>
+        <h4>Set Client:</h4>
+        <Dropdown placeholder="Set client" fluid search selection onChange={this.changeHandler} options={this.state.clientsDropDown}/>
+        <Button color="green" onClick={this.putClientAssociation}>Confirm</Button>
       </div>
     );
   }
@@ -66,17 +95,3 @@ class SingleProjectComponent extends Component {
 
 export default SingleProjectComponent;
 
-
-// import React from 'react'
-// import { Dropdown } from 'semantic-ui-react'
-
-// const options = [
-//   { key: 1, text: 'Choice 1', value: 1 },
-//   { key: 2, text: 'Choice 2', value: 2 },
-// ]
-
-// const DropdownExampleDisabled = () => (
-//   <Dropdown text='Dropdown' options={options} disabled />
-// )
-
-// export default DropdownExampleDisabled
--- src/RoutesCRUD/Practice/PracticeComponent.js
+++ src/RoutesCRUD/Practice/PracticeComponent.js
@@ -3,8 +3,21 @@ import './Practice.css'
 import GenericCrudComponent from '../Utils/GenericCrudComponent'
 import PracticeCard from './PracticeCard'
 import EditGenericCard from './EditGenericCard'
+import axios from 'axios'
 
 class PracticeComponent extends Component {
+  
+  componentDidMount(){
+    axios({  method: 'put',
+      url: 'http://localhost:8080/projects/11/client',
+      data: 'http://localhost:8080/clients/1',
+      headers:{'Content-Type':'text/uri-list'}})
+    .then((client)=>{
+      axios.get('http://localhost:8080/projects/10/client')
+      .then((client)=>{console.log(client.data)})
+    })
+  }
+
   render() {
     return (
       <div>
