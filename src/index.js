db43349ecec0c6a2c6257ad8fd8695ad279bf68b
William McCracken <wem5637@gmail.com>
2018-02-16 16:32:45 -0500

    Removes experimnetal item

--- src/index.js
+++ src/index.js
@@ -6,7 +6,5 @@ import 'semantic-ui-css/semantic.min.css';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import 'react-datepicker/dist/react-datepicker.css';
 
-setTimeout(() => {
-  ReactDOM.render(<App />, document.getElementById('root'));
-  registerServiceWorker();
-}, 1200);
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
